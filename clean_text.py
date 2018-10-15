b7bf001eb77d1b18c4201c212039815df8d5f5e2
Nethika Suraweera <nethika.suraweera@tinmankinetics.com>
2018-10-14 23:33:37 -0600

    2 methods

--- clean_text.py
+++ clean_text.py
@@ -1,6 +1,7 @@
 import re, string
+from string import ascii_lowercase
 
-def cleaned_text(text):
+def cleaned_text1(text):
     
     punctuation = ['!', ',', '.', ':', ';', '?']
     
@@ -16,6 +17,7 @@ def cleaned_text(text):
     
     # remove punctuation other than above list.
     exclude = str(set(string.punctuation) - set(punctuation))
+    print(exclude)
     regex = re.compile('[%s]' % re.escape(exclude))
     text = regex.sub(' ', text)
     
@@ -24,8 +26,29 @@ def cleaned_text(text):
 
     return text
 
+def cleaned_text2(text):
+    punctuation = ['!', ',', '.', ':', ';', '?']
+
+    # lower case
+    text = text.lower()
+
+    include = punctuation + list(ascii_lowercase) + [' ']
+
+    uniques = ''.join(set(text))
+    
+    # remove any character that is not in the list:include
+    for ch in uniques:
+        if ch not in include:
+            text = text.replace(ch, ' ')
+
+
+    #fix whitespaces
+    text = ' '.join(text.split())
+
+    return text
+
 if __name__ == "__main__":
         text = "### ï»¿project gutenberg's; , 7 by arthur conan doyle\n\nthis ebook is for the use of anyone anywhere at no cost and with\nalmost no restrictions whatsoever.  Hej då. hej d\xe5! TODO: fill > out + the function below that transforms the input text and window-size into a set of input/output pairs for use with our RNN model def window_transform_text(text, window_size, step_size):"
-        text = cleaned_text(text)
+        text = cleaned_text2(text)
         print("cleaned text:")
         print (text)
